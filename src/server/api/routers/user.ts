ee64890ca23dfdf1ef15047f4a4467c87508cdda
arvidbt <arvid.bergman.thorn@cygni.se>
2024-08-30 18:16:18 +0200

    add user router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,3 +1,34 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { db } from "@/server/db";
+import { userInfo } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+import {} from "date-fns";
 
-export const userRouter = createTRPCRouter({});
+export const userRouter = createTRPCRouter({
+  getUserInfo: protectedProcedure.mutation(async ({ ctx }) => {
+    if (!ctx.session.user.id) {
+      return null;
+    }
+    return await db.query.userInfo.findFirst({
+      where: eq(userInfo.createdById, ctx.session.user.id),
+    });
+  }),
+
+  setUserInfo: protectedProcedure
+    .input(z.object({ lifeExpectancy: z.number(), birthday: z.date() }))
+    .mutation(async ({ input, ctx }) => {
+      if (!ctx.session.user.id) {
+        return null;
+      }
+
+      await db
+        .insert(userInfo)
+        .values({
+          birthday: input.birthday.toDateString(),
+          lifeExpectancy: input.lifeExpectancy,
+          createdById: ctx.session.user.id,
+        })
+        .onConflictDoNothing();
+    }),
+});
